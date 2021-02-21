ec31fa39378cd53ec9b04359204fb5ef3e46d7e3
Tomoki Morimoto <morimoto.ke@gmail.com>
2021-02-22 01:59:01 +0900

    initial commit

---
+++ static/data.js
@@ -0,0 +1,67 @@
+// Simple 4-level hierarchy
+const simpleHierarchy = {
+  id: "39",
+  name: "39",
+  children: [
+    { id: "75", name: "75", children: [] },
+    { id: "69", name: "69", children: [{ id: "80", name: "80" }] },
+    {
+      id: "73",
+      name: "73",
+      children: [
+        {
+          id: "74",
+          name: "74",
+          children: [
+            { id: "77", name: "77" },
+            {
+              id: "91",
+              name: "91",
+              children: [
+                {
+                  id: "93",
+                  name: "93",
+                  children: [],
+                },
+                { id: "f07", name: "f07" },
+              ],
+            },
+            { id: "f06", name: "f06" },
+          ],
+        },
+      ],
+    },
+    {
+      id: "38",
+      name: "38",
+      children: [
+        { id: "65", name: "65" },
+        { id: "f08", name: "f08" },
+        {
+          id: "84",
+          name: "84",
+          children: [
+            {
+              id: "74",
+              name: "74",
+              children: [
+                { id: "77", name: "77" },
+                {
+                  id: "91",
+                  name: "91",
+                  children: [
+                    { id: "93", name: "93" },
+                    { id: "f07", name: "f07" },
+                  ],
+                },
+                { id: "f06", name: "f06" },
+              ],
+            },
+          ],
+        },
+        { id: "91", name: "91" },
+        { id: "92", name: "92" },
+      ],
+    },
+  ],
+};

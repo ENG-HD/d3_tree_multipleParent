c27de6c2a38e45ccb47f9f3f5bdd1c3d814a60cd
Tomoki Morimoto <morimoto.ke@gmail.com>
2021-02-22 20:08:32 +0900

    分類別の色分け追加

--- static/data.js
+++ static/data.js
@@ -2,18 +2,32 @@
 const simpleHierarchy = {
   id: "39",
   name: "39",
+  clasify: "A",
   children: [
-    { id: "75", name: "75", children: [] },
-    { id: "69", name: "69", children: [{ id: "80", name: "80" }] },
+    {
+      id: "75",
+      name: "75",
+
+      clasify: "B",
+      children: [],
+    },
+    {
+      id: "69",
+      name: "69",
+      clasify: "C",
+      children: [{ id: "80", name: "80", clasify: "B" }],
+    },
     {
       id: "73",
       name: "73",
+      clasify: "D",
       children: [
         {
           id: "74",
           name: "74",
+          clasify: "E",
           children: [
-            { id: "77", name: "77" },
+            { id: "77", name: "77", clasify: "A" },
             {
               id: "91",
               name: "91",
@@ -21,46 +35,50 @@ const simpleHierarchy = {
                 {
                   id: "93",
                   name: "93",
+                  clasify: "B",
                   children: [],
                 },
-                { id: "f07", name: "f07" },
+                { id: "f07", name: "f07", clasify: "C" },
               ],
             },
-            { id: "f06", name: "f06" },
+            { id: "f06", clasify: "D", name: "f06" },
           ],
         },
       ],
     },
     {
       id: "38",
+      clasify: "E",
       name: "38",
       children: [
-        { id: "65", name: "65" },
-        { id: "f08", name: "f08" },
+        { id: "65", clasify: "A", name: "65" },
+        { id: "f08", name: "f08", clasify: "A" },
         {
           id: "84",
           name: "84",
+          clasify: "C",
           children: [
             {
               id: "74",
               name: "74",
+              clasify: "S",
               children: [
-                { id: "77", name: "77" },
+                { id: "77", name: "77", clasify: "C" },
                 {
                   id: "91",
                   name: "91",
                   children: [
-                    { id: "93", name: "93" },
-                    { id: "f07", name: "f07" },
+                    { id: "93", name: "93", clasify: "B" },
+                    { id: "f07", name: "f07", clasify: "C" },
                   ],
                 },
-                { id: "f06", name: "f06" },
+                { id: "f06", name: "f06", clasify: "D" },
               ],
             },
           ],
         },
-        { id: "91", name: "91" },
-        { id: "92", name: "92" },
+        { id: "91", name: "91", clasify: "A" },
+        { id: "92", name: "92", clasify: "D" },
       ],
     },
   ],
